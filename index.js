6a6b1a8b1bafa2e1b0050f1e0be3bca3b1a551f7
AlexandreCSilva <alecosta.silva12@gmail.com>
2022-09-12 16:56:00 -0300

    ending the project

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from 'express';
 import cors from 'cors';
 import registerRouter from './routers/register.router.js';
 import authRouter from './routers/auth.router.js';
-import statusRouter from './routers/status.router.js';
 import balanceRouter from './routers/balance.router.js';
 import deleteRouter from './routers/delete.router.js';
 import editRouter from './routers/edit.router.js'
@@ -14,7 +13,6 @@ app.use(express.json());
 
 app.use(registerRouter);
 app.use(authRouter); 
-app.use(statusRouter);
 app.use(balanceRouter);
 app.use(deleteRouter);
 app.use(editRouter);
--- routers/status.router.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { status } from "../controllers/status.controller.js";
-
-const router = express.Router();
-
-router.post('/status', status);
-
-export default router;
